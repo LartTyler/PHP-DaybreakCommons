2ef7bbc224151d88dbdf983b989885e2a2a027f2
Tyler Lartonoix <larttyler@live.com>
2015-06-29 11:29:51 -0400

    Added an optional transformer to CSV fields

--- src/DaybreakStudios/Common/IO/CsvFileReader.php
+++ src/DaybreakStudios/Common/IO/CsvFileReader.php
@@ -3,12 +3,16 @@
 
 	class CsvFileReader extends FileReader {
 		protected $fields = [];
+		protected $transformers = [];
 
-		public function addField($name, $pos = null) {
+		public function addField($name, $pos = null, $transformer = null) {
 			if ($pos === null)
 				$pos = sizeof($this->fields);
 
 			$this->fields[$pos] = $name;
+
+			if (is_callable($transformer))
+				$this->transformers[$pos] = $transformer;
 
 			return $this;
 		}
@@ -21,11 +25,19 @@
 
 			if ($row === false || empty($this->fields))
 				return $row;
+			else if (sizeof($row) < sizeof($this->fields))
+				throw new IOException('One or more rows in your CSV have fewer columns than the number of fields');
 
 			$data = [];
 
-			foreach ($this->fields as $pos => $name)
-				$data[$name] = $row[$pos];
+			foreach ($this->fields as $pos => $name) {
+				$value = $row[$pos];
+
+				if (isset($this->transformers[$pos]))
+					$value = call_user_func($this->transformers[$pos], $value);
+
+				$data[$name] = $value;
+			}
 
 			return (object)$data;
 		}
